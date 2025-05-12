1aaac0b80379a16c755721bc4283dd52e775f397
Gaetano Leanza <leanzaundleanza@outlook.de>
2025-05-12 15:41:25 +0200

    add json and shopping card

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-  e.preventDefault(); // Formular nicht wirklich abschicken
+let cart = [];
+const dishes = [
+  {
+    id: 1,
+    name: "Pizza Margherita",
+    price: 9.9,
+    image: "  ",
+  },
+  {
+    id: 2,
+    name: "Pizza Salami",
+    price: 12.5,
+    image: "  ",
+  },
+  {
+    id: 3,
+    name: "Pizza Funghi",
+    price: 18.9,
+    image: "  ",
+  },
+  {
+    id: 4,
+    name: "Pizza Hawaii",
+    price: 8.5,
+    image: "  ",
+  },
+  {
+    id: 5,
+    name: "Pizza Quattro Formaggi",
+    price: 11.9,
+    image: "   ",
+  },
+];
 
-  // Spinner anzeigen
-  document.getElementById("spinner").style.display = "inline-block";
-
-  // Nach kurzer Zeit weiterleiten
-  setTimeout(function () {
-    window.location.href = "send_mail.html"; // Weiterleitung zur Bestätigungsseite
-  }, 1000); // 1 Sekunde simuliertes "Senden"
-});
